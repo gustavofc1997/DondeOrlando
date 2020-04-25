3e494717e9c983cb7f94c37460907bdf601082bf
Gustavo <gustavo.forero@globant.com>
2020-04-25 15:15:14 -0500

    update listener

--- app/src/main/java/com/gforeroc/dondeorlando/utils/PasswordDialogFragment.kt
+++ app/src/main/java/com/gforeroc/dondeorlando/utils/PasswordDialogFragment.kt
@@ -1,21 +1,18 @@
 package com.gforeroc.dondeorlando.utils
 
 import android.os.Bundle
-import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.Window
-import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.DialogFragment
-import com.gforeroc.dondeorlando.BR
 import com.gforeroc.dondeorlando.R
 import com.gforeroc.dondeorlando.databinding.DialogPasswordBinding
 import kotlinx.android.synthetic.main.dialog_password.*
 import kotlinx.android.synthetic.main.dialog_password.view.*
 
-class PasswordDialogFragment : DialogFragment() {
+class PasswordDialogFragment : DialogFragment(), OnNumberClickListener {
 
     private var window: Window? = null
     private var rootView: View? = null
@@ -25,24 +22,14 @@ class PasswordDialogFragment : DialogFragment() {
     ): View? {
         val binding: DialogPasswordBinding =
             DataBindingUtil.inflate(inflater, R.layout.dialog_password, container, false)
-        binding.listener = ClickHandler()
-        binding.context
+        binding.listener = this
         window = dialog?.window
         dialog?.requestWindowFeature(Window.FEATURE_NO_TITLE)
         return binding.root
     }
-    data class Number(var number: String)
-
-    class ClickHandler {
-        fun simpleButtonClick(view: View, number: Number) {
-            val values = view.et_code.text.toString()
-            values.plus(number)
-            view.et_code.setText(values)
-        }
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         close_dialog_password.setOnClickListener { dismiss() }
     }
 
@@ -51,4 +38,13 @@ class PasswordDialogFragment : DialogFragment() {
         dialog?.window?.setBackgroundDrawableResource(android.R.color.transparent)
         dialog?.window?.apply { setLayout(480, 630) }
     }
+
+    override fun onNumberClick(view: View, number: Int) {
+        val values =  et_code.text.toString().plus(number.toString())
+        et_code.setText(values)
+    }
+}
+
+interface OnNumberClickListener {
+    fun onNumberClick(view: View, number: Int)
 }
